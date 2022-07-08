0860117c1e0b9f06ff8e27e2a8fa6fe6b891b72d
istvancode <neuprond.istvan2@gmail.com>
2022-07-08 20:34:03 +0200

    App.js és App.test.js módosítása

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// ____________________________ ezt adtam hozzá
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.href).toContain('rbipartner.hu');
+});
